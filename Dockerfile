bf44cd9b8f9d870a916301c9f1b2fdd9709b2aaa
David Renz <david.renz@up2go.com>
2016-05-11 16:30:33 +0200

    Updated groovy script and some changes in the Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM anapsix/alpine-java
+FROM java
 
 MAINTAINER Up2Go-DavidRenz
 
 RUN cd /tmp && \
-	 wget http://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.6.zip && \ 
+     wget http://dl.bintray.com/groovy/maven/apache-groovy-binary-2.4.6.zip && \ 
 	 unzip apache-groovy-binary-2.4.6.zip && \ 
 	 mv groovy-2.4.6 /groovy  && \ 
 	 rm apache-groovy-binary-2.4.6.zip
@@ -11,7 +11,10 @@ RUN cd /tmp && \
 ENV GROOVY_HOME /groovy
 ENV PATH $GROOVY_HOME/bin/:$PATH
 
-RUN addgroup app -g 9000 && adduser -g 9000 -u 9000 -S -s /bin/false app 
+RUN groupadd app -g 9000 && useradd -g 9000 -u 9000 -r -s /bin/false app
+
+RUN apt-get remove -y curl wget bzr git mercurial openssh-client subversion bzip2 unzip xz-utils && \ 
+    apt-get autoremove -y 
 
 VOLUME /code
 WORKDIR /code
--- pmd.groovy
+++ pmd.groovy
@@ -19,25 +19,29 @@ def defaultRulesetLocation = scriptDir + "/apex-ruleset.xml"
 def customRulesetLocation = "/apex-ruleset.xml"
 if ( new File(customRulesetLocation).exists() ) {
     ruleset = customRulesetLocation
-} else {
+} 
+else {
     ruleset = defaultRulesetLocation
 }
+
+def pmdCommand = "${scriptDir}/lib/pmd/bin/run.sh pmd -d ${filesToAnalyse} -f codeclimate -R ${ruleset} -l apex -v 35"
 
 def sout = new StringBuffer()
 def serr = new StringBuffer()
 
-def pmdCommand = "${scriptDir}/lib/pmd/bin/run.sh pmd -d ${filesToAnalyse} -f codeclimate -R ${ruleset} -l apex -v 35".execute()
-pmdCommand.consumeProcessOutput(sout, serr)
-pmdCommand.waitFor()
-if (pmdCommand.exitValue() !=0 ) {
+def process = pmdCommand.execute()
+process.consumeProcessOutput(sout, serr)
+process.waitFor()
+if (process.exitValue() !=0 ) {
 	System.err << serr.toString()
+} 
+else {
+	System.out << sout.toString()
 }
-
-println sout
 
 def setupContext(cmdArgs) {
 	def cli = new CliBuilder(usage:"${this.class.name}")
-	cli._(longOpt: "configFile", required: true, args: 1, "Path to configuration json file")
+	cli._(longOpt: "configFile", required: true, args: 1, "Path to config.json file")
 	cli._(longOpt: "codeFolder", required: true, args: 1, "Path to code folder")
 	cli.parse(cmdArgs)
 }
